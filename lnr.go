a8921187e897c03f4fca615ccc9364b419a44116
Titus Tienaah <titus.intdxdt@gmail.com>
2017-10-20 01:36:17 -0300

    score fn

--- lnr.go
+++ lnr.go
@@ -4,11 +4,19 @@ import (
 	"simplex/rng"
 	"simplex/pln"
 	"github.com/intdxdt/geom"
+	"github.com/intdxdt/sset"
 )
+
+//score function
+type ScoreFn func(Linear, *rng.Range) (int, float64)
 
 //Linear interface
 type Linear interface {
 	Coordinates() []*geom.Point
 	Polyline() *pln.Polyline
 	Score(Linear, *rng.Range) (int, float64)
+}
+
+type SimpleAlgorithm interface {
+	Simple() *sset.SSet
 }
