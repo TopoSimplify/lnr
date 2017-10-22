49ba24a04ca5cfec6eaa2020bf2815b42874fc25
Titus Tienaah <titus.intdxdt@gmail.com>
2017-10-22 07:05:42 -0300

    interfaces

--- lnr.go
+++ lnr.go
@@ -3,20 +3,34 @@ package lnr
 import (
 	"simplex/rng"
 	"simplex/pln"
+	"simplex/opts"
 	"github.com/intdxdt/geom"
 	"github.com/intdxdt/sset"
-	"simplex/opts"
+	"github.com/intdxdt/deque"
 )
 
 //score function
 type ScoreFn func(Linear, *rng.Range) (int, float64)
 
-//Linear interface
-type Linear interface {
+type NodeQueue interface {
+	NodeQueue() *deque.Deque
+}
+
+type Polygonal interface {
 	Coordinates() []*geom.Point
 	Polyline() *pln.Polyline
+}
+
+type Lingen interface {
 	Score(Linear, *rng.Range) (int, float64)
 	Options() *opts.Opts
+}
+
+//Linear interface
+type Linear interface {
+	NodeQueue
+	Polygonal
+	Lingen
 }
 
 type SimpleAlgorithm interface {
