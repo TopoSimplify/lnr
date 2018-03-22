89dc051b35868b33398b4585f86555aaf75bf567
Titus Tienaah <titus.intdxdt@gmail.com>
2018-03-22 14:19:28 -0300

    refactor self

--- self.go
+++ self.go
@@ -10,94 +10,93 @@ import (
 	"github.com/intdxdt/rtree"
 )
 
-type kvCount struct {
-	count   int
-	indxset *sset.SSet
+type vertexDegree struct {
+	degree   int
+	indexSet *sset.SSet
 }
 
-type selfInter struct {
-	keyset *sset.SSet
+type crossings struct {
+	keySet *sset.SSet
 	point  *geom.Point
 }
 
-func updateKVCount(dict map[[2]float64]*kvCount, o *geom.Point, index int) {
-	var k = [2]float64{o[0], o[1]}
-	var v, ok = dict[k]
-	if !ok {
-		v = &kvCount{
-			count:   0,
-			indxset: sset.NewSSet(cmp.Int, 8),
-		}
-		dict[k] = v
-	}
-	v.indxset.Add(index)
-	v.count += 1
-}
-
 func SelfIntersection(polyline *pln.Polyline) *ctx.ContextGeometries {
-	var tree = *rtree.NewRTree(4)
-	var dict = make(map[[2]float64]*kvCount)
-	var data = make([]rtree.BoxObj, 0)
 
-	for _, s := range polyline.Segments() {
-		data = append(data, s)
-	}
-	tree.Load(data)
+	var nonPlanarDict = make(map[string]*crossings, 0)
+	var planarDict = make(map[[2]float64]*vertexDegree)
+	var tree, data = SegmentDB(polyline)
 
-	var selfIntersects = make(map[string]*selfInter, 0)
-
+	//update planar and non-planar dictionaries
 	for _, d := range data {
 		var s = d.(*seg.Seg)
-		var res = tree.Search(s.BBox())
-		updateKVCount(dict, s.A, s.I)
-		updateKVCount(dict, s.B, s.J)
-
-		for _, node := range res {
-			var otherSeg = node.GetItem().(*seg.Seg)
-			if s == otherSeg {
-				continue
-			}
-
-			var segG, otherSegG = s.Segment, otherSeg.Segment
-			var intersects = segG.Intersection(otherSegG)
-
-			if len(intersects) == 0 {
-				continue
-			}
-
-			for _, pt := range intersects {
-				if s.A.Equals2D(pt) || s.B.Equals2D(pt) {
-					continue
-				}
-				var skey = sset.NewSSet(cmp.Int, 8).Extend(
-					s.I, s.J, otherSeg.I, otherSeg.J,
-				)
-
-				var k = skey.String()
-				v, ok := selfIntersects[k]
-				if !ok {
-					v = &selfInter{keyset: skey, point: pt}
-				}
-				selfIntersects[k] = v
-			}
-		}
+		updatePlanarDict(planarDict, s)
+		updateNonPlanarDict(nonPlanarDict, s, tree)
 	}
 
 	var results = ctx.NewContexts()
-	for _, val := range selfIntersects {
-		cg := ctx.New(val.point, 0, -1).AsSelfNonVertex()
-		cg.Meta.SelfNonVertices = val.keyset
+
+	for _, val := range nonPlanarDict {
+		var cg = ctx.New(val.point, 0, -1).AsNonPlanarVertex()
+		cg.Meta.NonPlanarVertices = val.keySet
 		results.Push(cg)
 	}
 
-	for k, v := range dict {
-		if v.count > 2 {
-			i := v.indxset.First().(int)
-			cg := ctx.New(geom.NewPoint(k[:]), i, i).AsSelfVertex()
-			cg.Meta.SelfVertices = v.indxset
+	for k, v := range planarDict {
+		if v.degree > 2 {
+			var i = v.indexSet.First().(int)
+			var cg = ctx.New(geom.NewPoint(k[:]), i, i).AsPlanarVertex()
+			cg.Meta.PlanarVertices = v.indexSet
 			results.Push(cg)
 		}
 	}
 
 	return results.Sort()
+}
+
+func updatePlanarDict(dict map[[2]float64]*vertexDegree, s *seg.Seg) {
+	updateVertexDegree(dict, s.A, s.I)
+	updateVertexDegree(dict, s.B, s.J)
+}
+
+func updateNonPlanarDict(dict map[string]*crossings, s *seg.Seg, tree *rtree.RTree) {
+	var neighbours = tree.Search(s.BBox())
+	for _, node := range neighbours {
+		var o = node.GetItem().(*seg.Seg)
+		if s == o {
+			continue
+		}
+
+		var intersects = s.Intersection(o)
+
+		if len(intersects) == 0 {
+			continue
+		}
+
+		for _, pt := range intersects {
+			if s.A.Equals2D(pt) || s.B.Equals2D(pt) {
+				continue
+			}
+			var key = sset.NewSSet(cmp.Int, 8).Extend(s.I, s.J, o.I, o.J)
+			var k = key.String()
+			v, ok := dict[k]
+			if !ok {
+				v = &crossings{keySet: key, point: pt}
+			}
+			dict[k] = v
+		}
+	}
+}
+
+func updateVertexDegree(dict map[[2]float64]*vertexDegree, o *geom.Point, index int) {
+	var k = [2]float64{o[0], o[1]}
+	var v, ok = dict[k]
+	if !ok {
+		v = &vertexDegree{
+			degree:   0,
+			indexSet: sset.NewSSet(cmp.Int, 8),
+		}
+		dict[k] = v
+	}
+	v.indexSet.Add(index)
+	v.degree += 1
 }
---
+++ util.go
@@ -0,0 +1,22 @@
+package lnr
+
+import (
+	"simplex/pln"
+	"github.com/intdxdt/rtree"
+)
+
+func SegmentDB(polyline *pln.Polyline, nodeCapacity ...int ) (*rtree.RTree, []rtree.BoxObj) {
+	var capacity = 4
+	if len(nodeCapacity) > 0 {
+		capacity = nodeCapacity[0]
+	}
+
+	var tree = *rtree.NewRTree(capacity)
+	var data = make([]rtree.BoxObj, 0)
+	for _, s := range polyline.Segments() {
+		data = append(data, s)
+	}
+	tree.Load(data)
+
+	return tree, data
+}
